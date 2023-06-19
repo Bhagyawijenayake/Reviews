75e7cc2ed4e2761eca0c96fdac84b4d6afefdaf3
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-19 14:16:59 +0530

    add react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import people from './data';
+import{FaBeer} from 'react-icons/fa'
 
 
 const App = () => {
   const [index, setIndex] = useState(1);
   const { name, job, image, text } = people[index];
 console.log(name);
-  return <h2>Reviews Starter</h2>;
+  return <div>
+    <h2>Reviews dStarter</h2>
+    <FaBeer/>
+  </div>;
 };
 export default App;
