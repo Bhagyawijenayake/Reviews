e9c3a4113f32cf8d8e73263d43bdc38243303f90
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-20 10:59:54 +0530

    random person added

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,49 @@ import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
-  const [index, setIndex] = useState(1);
+  const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
   console.log(name);
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      return currentIndex + 1 > people.length - 1 ? 0 : currentIndex + 1;
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      return currentIndex - 1 < 0 ? people.length-1 : currentIndex -1 ;
+    });
+  };
+
+  const randomPerson= ()=>{
+    let randomNumber = Math.floor(Math.random()*people.length)
+    console.log(randomNumber);
+    if (randomNumber === index) {
+      randomNumber = index + 1>people.length-1?0:index+1;
+    }
+    setIndex(randomNumber)
+  }
   return (
     <main>
       <article className="review">
         <div className="img-container">
           <img src={image} alt={name} className="person-img" />
           <span className="quote-icon">
-            <FaQuoteRight/>
+            <FaQuoteRight />
           </span>
         </div>
-        <h4 className="author">{name}</h4>;
-        <p className="job">{job}</p>
+        <h4 className="author">{name}</h4>;<p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>suprise me</button>
       </article>
     </main>
   );
